ce4f565ba72fcdaa8cf628610ec1ca6989f66ac5
Karlo Biscan <biscan.karlo@gmail.com>
2018-03-01 15:23:55 +0100

    Added Routes for Main component

--- src/containers/main.js
+++ src/containers/main.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import Sidebar from './sidebar';
 import BeerList from './beer_list';
+import JoinUs from '../components/joinus/join_us';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 
 class Main extends Component {
   render() {
     return (
-      <div className="row">
-        <BeerList beer={this.props.beer} />
-        <Sidebar />
-      </div>
+        <Switch>
+          <Route exact path="/" render={()=><BeerList beer={this.props.beer} />}/>
+          <Route exact path="/favorites" render={()=><BeerList beer={this.props.beer} />}/>
+          <Route exact path="/join-us" component={JoinUs} />
+        </Switch>
     );
   }
 }
