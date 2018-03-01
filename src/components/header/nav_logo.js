150a77d089554a146c258a4c545180aeff4baec1
Karlo Biscan <biscan.karlo@gmail.com>
2018-03-01 21:08:07 +0100

    Added Logo images

--- src/components/header/nav_logo.js
+++ src/components/header/nav_logo.js
@@ -4,7 +4,10 @@ class NavLogo extends Component {
   render() {
     return (
       <div>
-        <a className="navbar-brand" href="#">Navbar</a>
+        <a className="navbar-brand" href="#">
+          <img src={require('../../assets/images/header/logo/symbol.png')} alt="logo" />
+          <img src={require('../../assets/images/header/logo/logo.png')} alt="logo" />
+        </a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
